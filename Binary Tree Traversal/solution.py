6776eba1d0c967b1423df887de9f7b79d1ce1cb7
Alexrlty <siu.pn@ucu.edu.ua>
2026-04-21 10:04:02 +0300

    Implement binary tree traversal methods

--- Binary Tree Traversal/solution.py
+++ Binary Tree Traversal/solution.py
@@ -1,13 +1,10 @@
 # Pre-order traversal
 def pre_order(node):
-    return []
+    return [] if not node else [node.data] + pre_order(node.left) + pre_order(node.right)
 
 # In-order traversal
 def in_order(node):
-    return []
-
+    return [] if not node else in_order(node.left) + [node.data] + in_order(node.right)
 # Post-order traversal
 def post_order(node):
-    return []
-
-
+    return [] if not node else post_order(node.left) + post_order(node.right) + [node.data]
